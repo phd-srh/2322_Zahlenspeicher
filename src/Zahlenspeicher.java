ee493b6420148e98f1ad9c4821ade03d966f84ea
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-03-21 14:22:14 +0100

    Attribute und Konstruktor ergänzt!

--- src/Zahlenspeicher.java
+++ src/Zahlenspeicher.java
@@ -1,2 +1,18 @@
 public class Zahlenspeicher {
+
+    // Attribute
+    private int kapazität;
+    private int[] speicher;
+
+    // Pre-Konstruktor (wird ausgeführt vor dem Aufruf des Konstruktors)
+    private int elementeImSpeicher = 0;
+
+    public Zahlenspeicher(int kapazität) {
+        this.kapazität = kapazität;
+        this.speicher = new int[ kapazität ];
+        //elementeImSpeicher = 0;
+    }
+
+    // Methoden
+
 }
